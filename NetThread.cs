100c0f918c7dca73b51bb08af5ae8ee95a1e15dd
chwuchang <305599233@qq.com>
2016-11-28 11:26:16 +0800

    Add files via upload

---
+++ NetThread.cs
@@ -0,0 +1,69 @@
+
+using System;
+using System.Threading;
+
+
+namespace Game.Network
+{
+    public sealed class NetThread
+    {
+        private Thread _thread;
+
+        private readonly Action _callback;
+
+        public int SleepTime;
+        private bool _running;
+        private readonly string _name;
+
+        public bool IsRunning
+        {
+            get { return _running; }
+        }
+
+        public NetThread(string name, int sleepTime, Action callback)
+        {
+            _callback = callback;
+            SleepTime = sleepTime;
+            _name = name;
+        }
+
+        public void Start()
+        {
+            if (_running)
+                return;
+            _running = true;
+            _thread = new Thread(ThreadLogic)
+            {
+                Name = _name,
+                IsBackground = true
+            };
+            _thread.Start();
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+                return;
+            _thread.Interrupt();
+            _running = false;
+
+            _thread.Join();
+        }
+        private void ThreadLogic()
+        {
+            while (_running)
+            {
+                _callback();
+                Thread.Sleep(SleepTime);
+            }
+            try
+            {
+               Thread.Sleep(SleepTime);
+            }
+            catch (ThreadInterruptedException e)
+           {
+               // NetworkDebug.Debug.Log(LogerType.INFO, "cannot go to sleep  interrupted by main thread" + e.Message);
+            }
+        }
+    }
+}
